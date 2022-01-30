87b295f723cc2c3f52065be4b63eb67ef3b9003e
Ritika Nagar <87242161+Ritika-Nagar@users.noreply.github.com>
2022-01-31 01:36:36 +0530

    Add files via upload

---
+++ pivot_sortedArray.java
@@ -0,0 +1,28 @@
+package arrays_questions;
+
+public class pivot_sortedArray {
+	public static int pivot(int [] arr) {
+		int s=0;
+		int e=arr.length-1;
+		int mid= s+(e-s)/2;
+		while(s<e) {
+		if(arr[mid]>arr[0]) {
+			s=mid+1;
+		}
+		else {
+			e=mid;
+		}
+		
+		mid= s+(e-s)/2;
+		}
+		return arr[e];
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] arr= {11,13,15,17};
+		
+		System.out.println(pivot(arr));
+	}
+
+}
